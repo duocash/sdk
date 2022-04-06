8a254437578731e89bcc6b709dac67ea81d4dc8a
xBA5ED <0xBA5ED@protonmail.com>
2022-04-07 00:29:49 +0200

    Initial commit

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../dist/umd'),
     filename: 'index.js',
-    library: 'exampleTypescriptPackage',
+    library: 'TypescriptPackage',
     libraryTarget: 'umd',
     globalObject: 'this',
   },
